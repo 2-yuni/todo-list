769e92e506a4ae25931eb96829a17b945d3dd694
2-yuni <kneettee@gmail.com>
2024-10-23 11:45:45 +0900

    Updates

---
+++ _next/static/SBcqubHtmUaznnlFy9toF/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/474-a51f83c9f051821e.js","static/css/ef46db3751d8e999.css","static/chunks/pages/index-76246ad4990676f7.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
